e0905b6f6a3723661915ef3b838e8d402ebc76ec
Will Bamford <w.bamford@gmail.com>
2016-08-16 06:37:02 +0100

    Experiment

--- dist/resl.js
+++ dist/resl.js
@@ -40,6 +40,102 @@ module.exports = {
 },{}],3:[function(require,module,exports){
 /* global XMLHttpRequest */
 
+var Loader = require('./Loader')
+var constants = require('./constants')
+var states = constants.states
+
+function get (request, assets, onError, onComplete, onProgress) {
+  var name = request.name
+  var stream = request.stream
+  var binary = request.type === 'binary'
+  var parser = request.parser
+
+  var xhr = new XMLHttpRequest()
+  var asset = null
+
+  var loader = new Loader(name, cancel)
+
+  if (stream) {
+    xhr.onreadystatechange = onReadyStateChange
+  } else {
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        onReadyStateChange()
+      }
+    }
+  }
+
+  if (binary) {
+    xhr.responseType = 'arraybuffer'
+  }
+
+  function onReadyStateChange () {
+    if (xhr.readyState < 2 ||
+        loader.state === states.COMPLETE ||
+        loader.state === states.ERROR) {
+      return
+    }
+    if (xhr.status !== 200) {
+      return onError('error loading resource "' + request.name + '"')
+    }
+    if (xhr.readyState > 2 && loader.state === states.DATA) {
+      var response
+      if (request.type === 'binary') {
+        response = xhr.response
+      } else {
+        response = xhr.responseText
+      }
+      if (parser.data) {
+        try {
+          asset = parser.data(response)
+        } catch (e) {
+          return onError(e)
+        }
+      } else {
+        asset = response
+      }
+    }
+    if (xhr.readyState > 3 && loader.state === states.DATA) {
+      if (parser.done) {
+        try {
+          asset = parser.done()
+        } catch (e) {
+          return onError(e)
+        }
+      }
+      loader.state = states.COMPLETE
+    }
+    assets[name] = asset
+    loader.progress = 0.75 * loader.progress + 0.25
+    loader.ready =
+      (request.stream && !!asset) ||
+      loader.state === states.COMPLETE
+    onProgress()
+  }
+
+  function cancel () {
+    if (loader.state === states.COMPLETE || loader.state === states.ERROR) {
+      return
+    }
+    xhr.onreadystatechange = null
+    xhr.abort()
+    loader.state = states.ERROR
+  }
+
+  // set up request
+  if (request.credentials) {
+    xhr.withCredentials = true
+  }
+  xhr.open('GET', request.src, true)
+  xhr.send()
+
+  return loader
+}
+
+module.exports = get
+
+},{"./Loader":1,"./constants":2}],4:[function(require,module,exports){
+var get = require('./get')
 var constants = require('./constants')
 var Loader = require('./Loader')
 var parameters = constants.parameters
@@ -92,93 +188,93 @@ module.exports = function resl (config) {
 
   var state = states.DATA
 
-  function loadXHR (request) {
-    var name = request.name
-    var stream = request.stream
-    var binary = request.type === 'binary'
-    var parser = request.parser
-
-    var xhr = new XMLHttpRequest()
-    var asset = null
-
-    var loader = new Loader(name, cancel)
-
-    if (stream) {
-      xhr.onreadystatechange = onReadyStateChange
-    } else {
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          onReadyStateChange()
-        }
-      }
-    }
-
-    if (binary) {
-      xhr.responseType = 'arraybuffer'
-    }
-
-    function onReadyStateChange () {
-      if (xhr.readyState < 2 ||
-          loader.state === states.COMPLETE ||
-          loader.state === states.ERROR) {
-        return
-      }
-      if (xhr.status !== 200) {
-        return abort('error loading resource "' + request.name + '"')
-      }
-      if (xhr.readyState > 2 && loader.state === states.DATA) {
-        var response
-        if (request.type === 'binary') {
-          response = xhr.response
-        } else {
-          response = xhr.responseText
-        }
-        if (parser.data) {
-          try {
-            asset = parser.data(response)
-          } catch (e) {
-            return abort(e)
-          }
-        } else {
-          asset = response
-        }
-      }
-      if (xhr.readyState > 3 && loader.state === states.DATA) {
-        if (parser.done) {
-          try {
-            asset = parser.done()
-          } catch (e) {
-            return abort(e)
-          }
-        }
-        loader.state = states.COMPLETE
-      }
-      assets[name] = asset
-      loader.progress = 0.75 * loader.progress + 0.25
-      loader.ready =
-        (request.stream && !!asset) ||
-        loader.state === states.COMPLETE
-      notifyProgress()
-    }
-
-    function cancel () {
-      if (loader.state === states.COMPLETE || loader.state === states.ERROR) {
-        return
-      }
-      xhr.onreadystatechange = null
-      xhr.abort()
-      loader.state = states.ERROR
-    }
-
-    // set up request
-    if (request.credentials) {
-      xhr.withCredentials = true
-    }
-    xhr.open('GET', request.src, true)
-    xhr.send()
-
-    return loader
-  }
+  // function get (request) {
+  //   var name = request.name
+  //   var stream = request.stream
+  //   var binary = request.type === 'binary'
+  //   var parser = request.parser
+  //
+  //   var xhr = new XMLHttpRequest()
+  //   var asset = null
+  //
+  //   var loader = new Loader(name, cancel)
+  //
+  //   if (stream) {
+  //     xhr.onreadystatechange = onReadyStateChange
+  //   } else {
+  //     xhr.onreadystatechange = function () {
+  //       if (xhr.readyState === 4) {
+  //         onReadyStateChange()
+  //       }
+  //     }
+  //   }
+  //
+  //   if (binary) {
+  //     xhr.responseType = 'arraybuffer'
+  //   }
+  //
+  //   function onReadyStateChange () {
+  //     if (xhr.readyState < 2 ||
+  //         loader.state === states.COMPLETE ||
+  //         loader.state === states.ERROR) {
+  //       return
+  //     }
+  //     if (xhr.status !== 200) {
+  //       return abort('error loading resource "' + request.name + '"')
+  //     }
+  //     if (xhr.readyState > 2 && loader.state === states.DATA) {
+  //       var response
+  //       if (request.type === 'binary') {
+  //         response = xhr.response
+  //       } else {
+  //         response = xhr.responseText
+  //       }
+  //       if (parser.data) {
+  //         try {
+  //           asset = parser.data(response)
+  //         } catch (e) {
+  //           return abort(e)
+  //         }
+  //       } else {
+  //         asset = response
+  //       }
+  //     }
+  //     if (xhr.readyState > 3 && loader.state === states.DATA) {
+  //       if (parser.done) {
+  //         try {
+  //           asset = parser.done()
+  //         } catch (e) {
+  //           return abort(e)
+  //         }
+  //       }
+  //       loader.state = states.COMPLETE
+  //     }
+  //     assets[name] = asset
+  //     loader.progress = 0.75 * loader.progress + 0.25
+  //     loader.ready =
+  //       (request.stream && !!asset) ||
+  //       loader.state === states.COMPLETE
+  //     notifyProgress()
+  //   }
+  //
+  //   function cancel () {
+  //     if (loader.state === states.COMPLETE || loader.state === states.ERROR) {
+  //       return
+  //     }
+  //     xhr.onreadystatechange = null
+  //     xhr.abort()
+  //     loader.state = states.ERROR
+  //   }
+  //
+  //   // set up request
+  //   if (request.credentials) {
+  //     xhr.withCredentials = true
+  //   }
+  //   xhr.open('GET', request.src, true)
+  //   xhr.send()
+  //
+  //   return loader
+  // }
 
   function loadElement (request, element) {
     var name = request.name
@@ -214,6 +310,7 @@ module.exports = function resl (config) {
     }
 
     function onComplete () {
+      console.log('ON COMPLETE');
       handleProgress()
       if (loader.state === states.DATA) {
         if (parser.done) {
@@ -292,10 +389,12 @@ module.exports = function resl (config) {
   }
 
   var loaders = {
-    text: loadXHR,
+    text: function (request) {
+      return get(request, assets, abort, onComplete, notifyProgress)
+    },
     binary: function (request) {
       // TODO use fetch API for streaming if supported
-      return loadXHR(request)
+      return get(request, assets, abort, onComplete, notifyProgress)
     },
     image: function (request) {
       return loadElement(request, document.createElement('img'))
@@ -440,5 +539,5 @@ module.exports = function resl (config) {
   }
 }
 
-},{"./Loader":1,"./constants":2}]},{},[3])(3)
+},{"./Loader":1,"./constants":2,"./get":3}]},{},[4])(4)
 });
